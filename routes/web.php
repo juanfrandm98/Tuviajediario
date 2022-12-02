5160db09d842d8e7b717a67390f78af56e4c96cd
juanfrandm98 <juanfrandm98@gmail.com>
2022-12-02 11:42:53 +0100

    Composer

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('testAlexa', function() {
-    return response('PROBANDO', 200)->header('Content-Type', 'text/plain');
+Route::get('/testAlexa', function() {
+	return view('welcome');
+    //return response('PROBANDO', 200)->header('Content-Type', 'text/plain');
 });
