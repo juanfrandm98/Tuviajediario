dae7755d62475b1be1134aa3cd75179868ef04ec
juanfrandm98 <juanfrandm98@gmail.com>
2022-12-02 11:05:48 +0100

    testAlexa

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testAlexa', function() {
+    return response('PROBANDO', 200)->header('Content-Type', 'text/plain');
+});
