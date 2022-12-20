bd088dbfba437dfa5de18585caa1d3289fe4b17e
juanfrandm98 <juanfrandm98@gmail.com>
2022-12-20 10:47:25 +0100

    Resultado: modificados los parámetros de addResultado

--- app/Http/Controllers/ResultadoController.php
+++ app/Http/Controllers/ResultadoController.php
@@ -8,7 +8,7 @@ use App\Models\Juego;
 
 class ResultadoController extends Controller
 {
-    public function addResultado(Request $request) {
+    public function addResultado2(Request $request) {
         $jsonParams = $request->get('json');
         $message = 'Error: fallo en los datos enviados.';
         $statusCode = 409;
@@ -38,6 +38,46 @@ class ResultadoController extends Controller
                 }
             } else {
                 $message = 'Error: no existe ningún juego con el código ' . $jsonResult['juego'] . '.';
+            }
+        }
+
+        return response($message, $statusCode)->header('Content-Type', 'text/plain');
+    }
+
+    public function addResultado(Request $request)
+    {
+        $codigo   = $request->get('codigo');
+        $jugador  = $request->get('jugador');
+        $fecha    = $request->get('fecha');
+        $puntos   = $request->get('puntos');
+        $segundos = $request->get('segundos');
+
+        $message = 'ERROR: Fallo en los datos enviados.';
+        $statusCode = 409;
+
+        if($codigo && $jugador && $fecha && $puntos && $segundos) {
+            if($juego = Juego::where('codigo', $codigo)->first()) {
+                $newResult = array(
+                    'juegoID'  => $juego->id,
+                    'jugador'  => $jugador,
+                    'fecha'    => $fecha,
+                    'puntos'   => $puntos,
+                    'segundos' => $segundos,
+                    'aviso'    => false
+                );
+
+                $newDBEntrance = new Resultado($newResult);
+                $newDBEntrance->save();
+
+                if($resultado = Resultado::where('juegoID', $juego->id)->where('jugador', $jugador)->
+                                           where('fecha', $fecha)->first()) {
+                    $message = 'Resultado introducido correctamente (ID: ' . $resultado->id . ').';
+                    $statusCode = 200;
+                } else {
+                    $message = 'ERROR: Los datos no se introdujeron en la BD correctamente.';
+                }
+            } else {
+                $message = 'ERROR: No existe ningún juego con el código ' . $codigo . '.';
             }
         }
 
