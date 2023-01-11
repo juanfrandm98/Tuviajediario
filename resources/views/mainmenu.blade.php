57c38d1f9b3eb50b0d299cc273f9da430c79351a
juanfrandm98 <juanfrandm98@gmail.com>
2023-01-11 11:33:29 +0100

    Web: Inicio Main Menu

--- resources/views/mainmenu.blade.php
+++ resources/views/mainmenu.blade.php
@@ -1,17 +1,5 @@
 <html lang="es">
-<head>
-    <title>Tu Viaje Diario - Menú Principal</title>
-    <meta charset="utf-8">
-</head>
-
-<body>
-    <div>
-        <h2>Menú Principal</h2>
-
-        <div>
-            <a> Lista de Juegos </a>
-            <a> Lista de Destinos </a>
-        </div>
-    </div>
-</body>
+    <body>
+        <h1>HELLO!</h1>
+    </body>
 </html>
