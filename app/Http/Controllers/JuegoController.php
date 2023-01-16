6ca1610e28b1309eb9dd56ccb809ef0fc4f92acf
juanfrandm98 <juanfrandm98@gmail.com>
2023-01-16 10:46:10 +0100

    JuegoController: Filtro de Test Simple y Campanadas

--- app/Http/Controllers/JuegoController.php
+++ app/Http/Controllers/JuegoController.php
@@ -47,7 +47,8 @@ class JuegoController extends Controller
         $tipoJuego = $request->get('tipo');
 
         if($tipoJuego) {
-            $juego = Juego::inRandomOrder()->where('tipo', $tipoJuego)->first();
+            //$juego = Juego::inRandomOrder()->where('tipo', $tipoJuego)->first();
+            $juego = Juego::inRandomOrder()->except([1,4])->where('tipo', $tipoJuego)->first();
 
             if($juego) {
                 $statusCode = 200;
