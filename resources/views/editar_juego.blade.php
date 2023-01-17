702acf2b990abead073bc9195095da4980bcb7e3
juanfrandm98 <juanfrandm98@gmail.com>
2023-01-17 11:53:50 +0100

    Web: Editar Juego

--- resources/views/editar_juego.blade.php
+++ resources/views/editar_juego.blade.php
@@ -17,11 +17,11 @@
         </h2>
 
         <form class="form_edicion_juego" method="get" action="{{route('editJuego')}}">
-            <label class="edit_label" for="nombre">Nombre del Juego:</label><br>
+            <label class="edit_label" for="nombre">Nombre del Juego:</label>
             <input type="text" name="nombre" id="nombre" class="small_text_input"
                    @if(isset($datos_iniciales)) value="{{$datos_iniciales->nombre}}" @endif>
 
-            <label class="edit_label" for="codigo">Código:</label><br>
+            <label class="edit_label" for="codigo">Código:</label>
             <input type="text" name="codigo" id="codigo" class="small_text_input"
                    @if(isset($datos_iniciales)) value={{$datos_iniciales->codigo}} @endif><br><br>
 
@@ -30,13 +30,13 @@
                 @if(isset($datos_iniciales)){{$datos_iniciales->explicacion}}@endif
             </textarea><br><br>
 
-            <label class="edit_label" for="tipo">Tipo:</label><br>
+            <label class="edit_label" for="tipo">Tipo:</label>
             <select name="tipo" id="tipo">
                 <option value="general">General</option>
                 <option value="especifico">Específico</option>
             </select>
 
-            <label class="edit_label" for="cota_inferior">Cota Inferdior (Puntos):</label><br>
+            <label class="edit_label" for="cota_inferior">Cota Inferdior (Puntos):</label>
             <input type="number" name="cota_inferior" id="cota_inferior" class="small_text_input"
                 @if(isset($datos_iniciales)) value="{{$datos_iniciales->cota_inferior}}"@endif><br><br>
         </form>
