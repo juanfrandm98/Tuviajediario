5e6096cb16eda343b120ae710db57a0412777fc8
juanfrandm98 <juanfrandm98@gmail.com>
2023-01-17 11:45:07 +0100

    Web: Editar Juego

--- resources/views/editar_juego.blade.php
+++ resources/views/editar_juego.blade.php
@@ -19,7 +19,7 @@
         <form class="form_edicion_juego" method="get" action="{{route('editJuego')}}">
             <label class="edit_label" for="nombre">Nombre del Juego:</label><br>
             <input type="text" name="nombre" id="nombre" class="small_text_input"
-                   @if(isset($datos_iniciales)) value="{{ucfirst($datos_iniciales->nombre)}}" @endif><br><br>
+                   @if(isset($datos_iniciales)) value="{{$datos_iniciales->nombre}}" @endif><br><br>
 
             <label class="edit_label" for="explicacion">Explicación:</label><br>
             <textarea name="explicacion" class="big_text_input" id="explicacion">
@@ -34,7 +34,7 @@
 
             <label class="edit_label" for="cota_inferior">Cota Inferior (Puntos):</label>
             <input type="number" name="cota_inferior" id="cota_inferior" class="small_text_input"
-                @if(isset($datos_iniciales)) value="{{ucfirst($datos_iniciales->cota_inferior)}}"@endif>
+                @if(isset($datos_iniciales)) value="{{$datos_iniciales->cota_inferior}}"@endif>
 
             <label class="edit_label" for="codigo">Código:</label>
             <input type="text" name="codigo" id="codigo" class="small_text_input"
