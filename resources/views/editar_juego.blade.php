a8c283c9ea82d0fa1908c7e699cdecfdb1bd23dd
juanfrandm98 <juanfrandm98@gmail.com>
2023-01-17 11:47:48 +0100

    Web: Editar Juego

--- resources/views/editar_juego.blade.php
+++ resources/views/editar_juego.blade.php
@@ -17,16 +17,20 @@
         </h2>
 
         <form class="form_edicion_juego" method="get" action="{{route('editJuego')}}">
-            <label class="edit_label" for="nombre">Nombre del Juego:</label><br>
+            <label class="edit_label" for="nombre">Nombre del Juego:</label>
             <input type="text" name="nombre" id="nombre" class="small_text_input"
-                   @if(isset($datos_iniciales)) value="{{$datos_iniciales->nombre}}" @endif><br><br>
+                   @if(isset($datos_iniciales)) value="{{$datos_iniciales->nombre}}" @endif>
+
+            <label class="edit_label" for="codigo">Código:</label>
+            <input type="text" name="codigo" id="codigo" class="small_text_input"
+                   @if(isset($datos_iniciales)) value={{$datos_iniciales->codigo}} @endif><br><br>
 
             <label class="edit_label" for="explicacion">Explicación:</label><br>
             <textarea name="explicacion" class="big_text_input" id="explicacion">
                 @if(isset($datos_iniciales)){{$datos_iniciales->explicacion}}@endif
             </textarea><br><br>
 
-            <label for="tipo">Tipo:</label>
+            <label class="edit_label" for="tipo">Tipo:</label>
             <select name="tipo" id="tipo">
                 <option value="general">General</option>
                 <option value="especifico">Específico</option>
@@ -34,11 +38,7 @@
 
             <label class="edit_label" for="cota_inferior">Cota Inferior (Puntos):</label>
             <input type="number" name="cota_inferior" id="cota_inferior" class="small_text_input"
-                @if(isset($datos_iniciales)) value="{{$datos_iniciales->cota_inferior}}"@endif>
-
-            <label class="edit_label" for="codigo">Código:</label>
-            <input type="text" name="codigo" id="codigo" class="small_text_input"
-                @if(isset($datos_iniciales)) value={{$datos_iniciales->codigo}} @endif>
+                @if(isset($datos_iniciales)) value="{{$datos_iniciales->cota_inferior}}"@endif><br><br>
         </form>
     </div>
 @endsection
