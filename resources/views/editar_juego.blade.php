8e49117fb0497a3d41c9427d547c9213c7a60e14
juanfrandm98 <juanfrandm98@gmail.com>
2023-01-18 13:29:37 +0100

    Áreas cognitivas en EditJuego

--- resources/views/editar_juego.blade.php
+++ resources/views/editar_juego.blade.php
@@ -60,7 +60,7 @@
                             @foreach($areas as $area)
                                 <td>
                                     <label><input type="checkbox" id="{{$area->id}}_check" name="{{$area->id}}_check"
-                                        @if(in_array($area->id, $datos_iniciales->$areas_cognitivas)) checked @endif>
+                                        @if(in_array($area->id,$datos_iniciales->areas_cognitivas)) checked @endif>
                                         {{$area->nombre}}
                                     </label>
                                 </td>
