d05633c82fd0fba32a25258abffa97cafbebe5d7
juanfrandm98 <juanfrandm98@gmail.com>
2023-01-19 11:47:39 +0100

    Migración de usuarios

--- database/migrations/2023_01_19_103157_create_usuarios_table.php
+++ database/migrations/2023_01_19_103157_create_usuarios_table.php
@@ -16,6 +16,23 @@ class CreateUsuariosTable extends Migration
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('email')->unique();
+            $table->string('contrasenia');
+            $table->string('nombre');
+            $table->string('telefono');
+            $table->enum('genero', ['hombre', 'mujer', 'otro']);
+            $table->integer('tipoUsuario');
+            $table->json('tutela')->nullable();
+            $table->json('gestiona')->nullable();
+            $table->string('alias');
+            $table->string('nombre_madre');
+            $table->json('resultados')->nullable();
+            $table->json('ultimos_viajes')->nullable();
+        });
+
+        Schema::table('resultados', function (Blueprint $table) {
+            $table->integer('jugador')->change();
+            $table->renameColumn('jugador', 'jugadorID');
         });
     }
 
