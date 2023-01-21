41830b738821da09582adcc2690d75a2e265a615
juanfrandm98 <juanfrandm98@gmail.com>
2023-01-21 13:02:11 +0100

    Arreglo MainMenu

--- resources/views/mainmenu.blade.php
+++ resources/views/mainmenu.blade.php
@@ -9,16 +9,13 @@
         <div id="opciones_menu_principal">
             <a href="{{route('lista_resultados')}}">Resultados de los tutelados</a>
 
-            @if(session('usuarioRolID' != 2))
+            @if(session('usuarioRolID') != 2)
                 <a href="{{route('lista_juegos')}}">Lista de Juegos</a>
                 <a href="{{route('lista_areas_cognitivas')}}">Lista de Áreas Cognitivas</a>
             @endif
 
-            @if(session('usuarioRolID' == 1))
+            @if(session('usuarioRolID') == 1)
                 <a href="{{route('lista_destinos')}}">Lista de Destinos</a>
-            @endif
-
-            @if(session('usuarioRolID' == 1))
                 <a href="{{route('registrar_otro_usuario')}}">Registrar Usuario</a>
             @else
                 <a href="{{route('registrar_otro_usuario')}}">Registrar Jugador</a>
