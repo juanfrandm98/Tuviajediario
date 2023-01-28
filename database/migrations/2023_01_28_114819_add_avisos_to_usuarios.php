5aaf7cc550c835c9544ab7b11c18de7b921b8eb2
juanfrandm98 <juanfrandm98@gmail.com>
2023-01-28 12:49:41 +0100

    Modificación de la migracion de usuarios

--- database/migrations/2023_01_28_114819_add_avisos_to_usuarios.php
+++ database/migrations/2023_01_28_114819_add_avisos_to_usuarios.php
@@ -14,7 +14,7 @@ class AddAvisosToUsuarios extends Migration
     public function up()
     {
         Schema::table('usuarios', function (Blueprint $table) {
-            //
+            $table->json('avisos')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddAvisosToUsuarios extends Migration
     public function down()
     {
         Schema::table('usuarios', function (Blueprint $table) {
-            //
+            $table->dropColumn('avisos');
         });
     }
 }
